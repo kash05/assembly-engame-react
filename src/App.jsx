267225c2039a5b9a8331f1fae46d3b6b8e6e9eb2
kash05 <walkingstatue11@gmail.com>
2025-06-20 10:52:17 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src="/vite.svg" className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.jsx</code> and save to test HMR
+				</p>
+			</header>
+		</div>
+	)
+}
